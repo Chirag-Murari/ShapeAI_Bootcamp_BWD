c9bf20ad5cf952cd2fa46ce4d63067922d711c31
Chirag-Murari <84890563+Chirag-Murari@users.noreply.github.com>
2021-06-07 11:49:10 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and react.js</h1>
+      <p>
+        JavaScript is the world's most popular programming language. JavaScript
+        is the programming language of the Web. JavaScript is easy to learn.
+        This tutorial will teach you JavaScript from basic to advanced
+      </p>
+    </div>
+  );
+}
+
+export default Note;
